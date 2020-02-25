80d365df12ba4550fba8d9e1e8476ca7aaa982fd
med-iv <iv.medvedev179@gmail.com>
2020-02-25 23:05:34 +0300

    First part changed

--- database/populatedb.sql
+++ database/populatedb.sql
@@ -1,40 +1,33 @@
 INSERT INTO Employees (Employee_id, Name, Last_name, Position, Login, Password) VALUES
- (1, 'Екатерина', 'Шульман', 'lawyer', 'Shulman', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5'),
- (2, 'Владимир', 'Путин', 'lawyer', 'Putin', '3bced02e0341dad74c304d06c4c1f69efb9aab8c148b327bc71d7ec08e252a42'),
- (3, 'Василий', 'Пупкин', 'intern', 'Pupkin', '24ae0794fd20c797223700756cd4d422ce34a4d06967d2f590945d0f13c29d21');
+ (1, 'Геральд', 'Ривов', 'lawyer', 'Vedmak', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5'),
+ (2, 'Владимир', 'Иванов', 'lawyer', 'Ivanov', '3bced02e0341dad74c304d06c4c1f69efb9aab8c148b327bc71d7ec08e252a42'),
+ (3, 'Василий', 'Кустиков', 'intern', 'Bush', '24ae0794fd20c797223700756cd4d422ce34a4d06967d2f590945d0f13c29d21');
 
 
  INSERT INTO Services (Service_id, Service_name) VALUES
- (1, 'concluded agreement'),
- (2, 'payment'),
- (3, 'call'),
- (4, 'meeting');
+ (1, 'Marriage Separation Agreement'), --развод
+ (2, 'Simple Real Estate Lease'), --простая аренда
+ (3, 'Power of Attorney'), --доверенность
+ (4, 'Bankruptcy Worksheet'); -- лист банкротства
 
  INSERT INTO History (Event_id, Service_id, Date, Description, Contract) VALUES
- (1, 2, '01/12/2019', 'Покупка недвижимости в Москве для ', 'https://blank-dogovor-kupli-prodazhi.ru/'),
- (2, 3, '05/09/2019', 'Звонок клиента с просьбой расмотреть дело сети', ''),
- (3, 3, '31/12/2019', 'Холодный звонок', '');
+ (1, 2, '01/12/2019', 'Аренда квартиры в Бутово', 'file:///kvartira_butovo.docx'),
+ (2, 1, '05/09/2019', 'Развод со скандалом', 'file:///razvod_ivanovs.docx'),
+ (3, 4, '31/12/2019', '', 'file:///bankrot_shlyapik.docx');
 
- INSERT INTO History (Event_id, Service_id, Date, Contract) VALUES
- (4, 4, '01/01/2000', 'http://www.consultant.ru/document/cons_doc_LAW_346055/'),
- (5, 4, '23/02/2005', 'http://www.consultant.ru/document/cons_doc_LAW_305/');
 
  INSERT INTO Clients (Client_id, Name, Last_name, Phone_number) VALUES
- (1, 'Жока', 'Валиев', '8(800)555-35-35'),
- (2, 'Otis', 'Lindeman', '+3(875)123-67-87'),
- (3, 'Татьяна', 'Финкельман', '8(987)665-43-21');
+ (1, 'Дмитрий', 'Иванов', '8(800)555-35-35'),
+ (2, 'Ольга', 'Иванова', '+3(875)123-67-87'),
+ (3, 'Татьяна', 'Финкельман', '8(987)665-43-21'),
+ (4, 'Александр', 'Шляпик', '');
 
- INSERT INTO Clients (Client_id, Name, Middle_name, Last_name, Phone_number, Email) VALUES
- (4, 'Артемий', 'Aндреевич', 'Лебедев', '8(100)500-14-88', 'tema@tema.ru'),
- (5, 'Carl', 'ALBUS', 'Coral', '5050401', 'carl@yahoo.com'),
- (6, 'Валерий', 'Альбертович', 'Пистолетов', '456-78-12', 'pis4on@asd1.ua');
 
  INSERT INTO History_clients (Client_id, Event_id) VALUES
- (1, 1),
- (2, 3),
- (4, 4),
- (4, 1),
- (2, 4);
+ (1, 2),
+ (2, 2),
+ (3, 1),
+ (4, 3);
 
  INSERT INTO History_employees (Event_id, Employee_id) VALUES
  (1, 3),
